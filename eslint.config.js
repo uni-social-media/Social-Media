e88db8d2b40a27f54691d783ae27a40c8a7ed747
MahderK <mahderkassaw@gmail.com>
2026-06-25 11:36:12 +0300

    ran format:write to codebase

--- eslint.config.js
+++ eslint.config.js
@@ -4,22 +4,18 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default [
   // Use recommended ESLint rules
   eslint.configs.recommended,
-  
+
   // Turn off rules that conflict with Prettier (must be last)
   eslintConfigPrettier,
 
   {
     // Ignore files and directories
-    ignores: [
-      "**/node_modules/",
-      "**/dist/",
-      "**/build/",
-    ],
+    ignores: ['**/node_modules/', '**/dist/', '**/build/'],
   },
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off"
-    }
-  }
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
 ];

59080467e10c56bcc30966747f8990cc653cc7d1
MahderK <mahderkassaw@gmail.com>
2026-06-25 10:35:44 +0300

    integrate ESLint and Prettier to both CI and local projects

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  // Use recommended ESLint rules
+  eslint.configs.recommended,
+  
+  // Turn off rules that conflict with Prettier (must be last)
+  eslintConfigPrettier,
+
+  {
+    // Ignore files and directories
+    ignores: [
+      "**/node_modules/",
+      "**/dist/",
+      "**/build/",
+    ],
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];

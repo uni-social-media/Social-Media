d345e58088422e7eabda0ab72efbc26d070484b8
MahderK <mahderkassaw@gmail.com>
2026-07-02 09:00:49 +0300

    fix: remove unused attribute

--- apps/server/models/User.js
+++ apps/server/models/User.js
@@ -47,7 +47,7 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
 });

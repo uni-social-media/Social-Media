d522491a14c08a40fb5c603c6d0b4e65507159d0
MahderK <mahderkassaw@gmail.com>
2026-07-02 08:49:55 +0300

    fix: added forgoten next(); statement

--- apps/server/models/User.js
+++ apps/server/models/User.js
@@ -50,6 +50,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 userSchema.methods.comparePassword = async function (candidate) {
